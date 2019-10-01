16bb874208fb25ece254eaa39243dd71056fe99c
ryohei-yoshimoto <vkobe7@icloud.com>
2019-10-01 08:40:24 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 //トップページ
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -26,4 +24,7 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ユーザ機能
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+
 });
+
